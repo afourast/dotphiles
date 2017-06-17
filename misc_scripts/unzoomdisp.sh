0457b4d9fbe6abf1eeb56ea011c2825be9a4a057
Triantafyllos Afouras <afourast@gmail.com>
2017-06-17 19:39:58 +0100

    tigrc and misc scripts

---
+++ misc_scripts/unzoomdisp.sh
@@ -0,0 +1,5 @@
+gsettings set org.gnome.desktop.interface scaling-factor 1
+xrandr --output eDP1 --scale 1x1
+PYCHARM_PATH=~/.PyCharm2016.2
+OPT_FILEPATH=$PYCHARM_PATH/config/options/colors.scheme.xml
+sed -i 's/zoomed/Default copy/g' $OPT_FILEPATH
